cc489d4a712c57b0a73df083246a5b81c65419f4
Surendra Babu G <surendrababuguram@gmail.com>
2021-09-17 13:33:13 +0530

    Create install_tarball.sh

---
+++ install_tarball.sh
@@ -0,0 +1,6 @@
+pip install sagemaker_studio_autoshutdown-0.1.1.tar.gz 
+jlpm config set cache-folder /tmp/yarncache
+jupyter lab build --debug --minimize=False
+echo "#######################################################"
+echo "The installation was successful. This terminal window will close in 10 seconds. Refresh your IDE to see the extension on the sidebar."
+sleep 10
